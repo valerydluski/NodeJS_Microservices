9692328e5f9b221477bad63e87e2f08588fbb5de
Valery Dluski <valerydluski@gmail.com>
2021-03-01 17:29:47 +0300

    feat: Querying the registry

--- service-registry/server/service.js
+++ service-registry/server/service.js
@@ -33,7 +33,12 @@ module.exports = (config) => {
     return res.json({ result: serviceKey });
   });
 
-  service.get('/find/:servicename/:serviceversion', (req, res, next) => next('Not implemented'));
+  service.get('/find/:servicename/:serviceversion', (req, res) => {
+    const { servicename, serviceversion } = req.params;
+    const svc = serviceRegistry.get(servicename, serviceversion);
+    if (!svc) return res.status(404).json({ result: 'Service not found' });
+    return res.json(svc);
+  });
 
   // eslint-disable-next-line no-unused-vars
   service.use((error, req, res, next) => {
